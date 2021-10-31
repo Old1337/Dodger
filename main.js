8ff7f852f0ed2f8022fbb1316b03498407e7b74c
Arsenic-33 <omarahmed2441@gmail.com>
2021-10-31 07:25:33 +0200

    added more js and animation to some section

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-porgrass = document.querySelectorAll(".prograss span");
+let porgrass = document.querySelectorAll(".prograss span");
 
 window.onscroll = function () {
-  if (scrollY >= 7930) {
+  if (scrollY >= 7730) {
     porgrass.forEach((element) => {
       element.style.width = element.getAttribute("data");
-      console.log(scrollY);
     });
   }
 };
+
+let numbers = document.querySelectorAll(".statistics .number");
+
+setInterval(function () {
+  if (scrollY >= 12496) {
+    numbers.forEach((element) => {
+      if (+element.innerHTML >= element.getAttribute("value")) {
+        clearInterval();
+      } else {
+        element.innerHTML++;
+      }
+    });
+  }
+}, 1);
