242cef2c501ce3d72c5e38ad9f7608c83209f889
Arsenic-33 <omarahmed2441@gmail.com>
2022-01-08 21:32:40 +0200

    fixed some stuff

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ let counter = setInterval(() => {
   document.querySelector(".events .seconds").innerHTML =
     seconds < 10 ? `0${seconds}` : seconds;
 
-  if (dateDiff < 0) {
+  if (dateDiff <= 0) {
     clearInterval(counter);
   }
 }, 1000);
